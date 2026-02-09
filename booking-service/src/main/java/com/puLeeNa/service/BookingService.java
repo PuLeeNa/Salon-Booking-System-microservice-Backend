e1cbc6af3a262401d942852aaa4bcac759325bb4
Your Name <wickramasooriyabgpv.22@uom.lk>
2026-02-09 16:04:13 +0530

    Add booking service implementation; create Booking, BookingStatus, and related DTOs

---
+++ booking-service/src/main/java/com/puLeeNa/service/BookingService.java
@@ -0,0 +1,26 @@
+package com.puLeeNa.service;
+
+import com.puLeeNa.domain.BookingStatus;
+import com.puLeeNa.dto.BookingRequest;
+import com.puLeeNa.dto.SalonDTO;
+import com.puLeeNa.dto.ServiceDTO;
+import com.puLeeNa.dto.UserDTO;
+import com.puLeeNa.modal.Booking;
+import com.puLeeNa.modal.SalonReport;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+public interface BookingService {
+
+    Booking createBooking(BookingRequest booking, UserDTO user, SalonDTO salon, Set<ServiceDTO> serviceDTOSet);
+
+    List<Booking> getBookingsByCustomer(Long customerId);
+    List<Booking> getBookingsBySalon(Long salonId);
+    Booking getBookingById(Long id);
+    Booking updateBooking(Long bookingId, BookingStatus status);
+    List<Booking> getBookingsByDate(LocalDate date, Long salonId);
+    SalonReport getSalonReport(Long salonId);
+}
